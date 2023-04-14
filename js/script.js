0d210ce789860c7f3d68a1f27b8a7ba35ed49145
David Santana <91860681+David-Santana98@users.noreply.github.com>
2023-04-14 16:58:57 -0300

    quak opção seguro esta selecionada

--- js/script.js
+++ js/script.js
@@ -12,3 +12,16 @@ function ativarLink(link) {
 links.forEach(ativarLink);
 
 // Ativar Ìtems do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativaProduto(parametro) {
+    const elemento = document.getElementById(parametro)
+    if (elemento) {
+    elemento.checked = true;
+    }
+    console.log(elemento);
+}
+
+parametros.forEach(ativaProduto)
+
